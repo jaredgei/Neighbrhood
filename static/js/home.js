c6a65c4319ec9a0b427b3d292014c49b12aeb993
Jared Geilich <jgeilich@grokker.com>
2015-08-19 14:41:43 -0700

    bug

--- static/js/home.js
+++ static/js/home.js
@@ -2,7 +2,7 @@ var ref = new Firebase("https://scorching-heat-6427.firebaseIO.com");
 var authData = ref.getAuth();
 
 if (authData) {
-    $('.welcome').html('Welcome, ' + authData.uid);
+        console.log('authenticated');
 } else {
     window.location = "login.html";
 }
